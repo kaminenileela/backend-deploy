81b69502a6afbf387132f0568ed47c5f9faa312f
kaminenileela <kaminenileela1992@email.com>
2024-08-31 17:35:07 -0400

    expense

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -3,6 +3,6 @@ component=$1
 environment=$2
 app_Version=$3
 dnf install ansible -y
-pip 3.9 install botocore boto3
+pip install botocore boto3
 ansible-pull -i localhost, -U https://github.com/kaminenileela/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
 #appVersion(this name is what we gave in app-pre-req.yaml)=$appVersion (this is what we defined for $3)
